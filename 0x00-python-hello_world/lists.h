9963177d40600e3acad83cac745f755cd9a7e786
ZezetuSibara <zezethu0730@gmail.com>
2023-06-05 14:45:40 -0700

    0x00. Python - Hello, World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -11,16 +11,15 @@
  * Descr: node structure for singly linked list
  * used in Holberton projects
  */
-int check_cycle(listint_t *list);
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
