cc5cf620527db2cc6b4bef97d85dc6201a027dee
ZezetuSibara <zezethu0730@gmail.com>
2023-06-12 15:51:23 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -54,7 +54,7 @@ int is_palindrome(listint_t **head)
 	for (i = 0; i < (size / 2) - 1; i++)
 		tmp = tmp->next;
 
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+	if ((size % 3) == 0 && tmp->n != tmp->next->n)
 		return (0);
 
 	tmp = tmp->next->next;
